3a9fdb7220120c3da9a1191ceddaec98d3a25332
Gars <vladimir.feofilaktov@altarix.ru>
2015-10-20 12:02:45 +0300

    push

--- app/src/main/java/org/shikimori/client/MainActivity.java
+++ app/src/main/java/org/shikimori/client/MainActivity.java
@@ -1,10 +1,16 @@
 package org.shikimori.client;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import org.shikimori.client.activity.DrawerActivity;
 import org.shikimori.client.adapters.DrawerAdapter;
+import org.shikimori.client.gsm.RegistrationIntentService;
 import org.shikimori.library.features.profile.InboxFragment2;
 import org.shikimori.library.fragments.UserNewsFragment;
 import org.shikimori.library.tool.constpack.Constants;
@@ -16,6 +22,8 @@ import java.util.List;
  * Created by Владимир on 20.06.2014.
  */
 public class MainActivity extends DrawerActivity {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,12 @@ public class MainActivity extends DrawerActivity {
         loadPage(DrawerAdapter.DRAWER_MENU_PROFILE_ID);
         drawerTool.setSelected(DrawerAdapter.DRAWER_MENU_PROFILE_ID);
         setDowbleBackPressetToExit(true);
+
+//        if (checkPlayServices()) {
+//            // Start IntentService to register this application with GCM.
+//            Intent intent = new Intent(this, RegistrationIntentService.class);
+//            startService(intent);
+//        }
     }
 
     public Fragment getExtraPage() {
@@ -59,5 +73,26 @@ public class MainActivity extends DrawerActivity {
                 }
             }
         }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(getClass().getSimpleName(), "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
