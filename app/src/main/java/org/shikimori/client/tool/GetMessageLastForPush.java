2e8ff98a62f31b3fc30adba24f9eef8c9e939c43
Gars <vladimir.feofilaktov@altarix.ru>
2016-04-08 12:24:52 +0300

    push

--- app/src/main/java/org/shikimori/client/tool/GetMessageLastForPush.java
+++ app/src/main/java/org/shikimori/client/tool/GetMessageLastForPush.java
@@ -1,7 +1,9 @@
 package org.shikimori.client.tool;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -30,36 +32,47 @@ public class GetMessageLastForPush {
                 JSONArray rezult = res.getResultArray();
                 if(rezult!=null){
                     final ItemDialogs item = new ItemDialogs().create(rezult.optJSONObject(0));
-
-//                     if message from self
-                    if(item.message!=null && item.message.from!=null){
-                        if(ShikiUser.USER_ID.equals(item.message.from.id))
-                            return;
-                    }
-                    // load user avatar
-                    if(item.user!=null && item.user.img148!=null){
-                        ImageLoader.getInstance().loadImage(ProjectTool.fixUrl(item.user.img148),
-                                new SimpleImageLoadingListener(){
-                                    @Override
-                                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                                        // send notify
-
-                                        Bundle b = new Bundle();
-                                        b.putString(PushHelperReceiver.MSG_TITLE, item.user.nickname);
-                                        b.putString(PushHelperReceiver.MSG_BODY, item.message.body);
-                                        b.putParcelable(PushHelperReceiver.PushAction.LARGE_ICON, loadedImage);
-                                        PushHelperReceiver.notifyUser(
-                                                query.getContext(),
-                                                PushHelperReceiver.getAction(ShikiApplikation.PRIVATE),
-                                                b);
-
-//                                        new PushHelperShiki(query.getContext())
-//                                                .sendLastMessage(item.user.nickname, item.message.body, loadedImage);
-                                    }
-                                });
-                    }
+                    notifyFrom(query.getContext(), item);
                 }
             }
         });
+    }
+
+    public static void notifyFrom(final Context context, final ItemDialogs item){
+        if(item.message!=null && item.message.from!=null){
+            if(ShikiUser.USER_ID.equals(item.message.from.id))
+                return;
+        }
+        // load user avatar
+        if(item.user!=null){
+            notifyFrom(context, item.user.nickname, item.message.body, item.user.img148);
+        }
+    }
+
+    public static void notifyFrom(final Context context, final String nick, final String text, String imgUrl){
+        if(!TextUtils.isEmpty(imgUrl)) {
+            ImageLoader.getInstance().loadImage(ProjectTool.fixUrl(imgUrl),
+                    new SimpleImageLoadingListener(){
+                        @Override
+                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                            // send notify
+                            sendMessage(context, nick, text, loadedImage);
+                        }
+
+                    });
+        } else {
+            sendMessage(context, nick, text, null);
+        }
+    }
+
+    private static void sendMessage(final Context context, String nick, String text, Bitmap bitmap){
+        Bundle b = new Bundle();
+        b.putString(PushHelperReceiver.MSG_TITLE, nick);
+        b.putString(PushHelperReceiver.MSG_BODY, text);
+        b.putParcelable(PushHelperReceiver.PushAction.LARGE_ICON, bitmap);
+        PushHelperReceiver.notifyUser(
+                context,
+                PushHelperReceiver.getAction(ShikiApplikation.PRIVATE),
+                b);
     }
 }
